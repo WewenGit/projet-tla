c9820117ad58c1d43f5b583d9fd003c0e08e4da9
lulug <zeyzer37@gmail.com>
2024-01-10 16:21:21 +0100

    Update Scene.java

--- src/Scene.java
+++ src/Scene.java
@@ -1,24 +1,30 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Scene {
     private String text;
     private boolean isEnd;
     private int id;
     private ArrayList<Choice> choices = new ArrayList<>();
+    private HashMap<String,Boolean> conditionsToChange = new HashMap<String,Boolean>();
     
-    public Scene(int id, String text, ArrayList<Choice> choices, boolean isEnd) {
+    public Scene(int id, String text, ArrayList<Choice> choices, HashMap<String,Boolean> conditionsToChange, boolean isEnd) {
         this.id=id;
         this.text = text;
         this.choices = choices;
+        this.conditionsToChange = conditionsToChange;
         this.isEnd = isEnd;
     }
     
-    public ArrayList<Choice> getChoices()
-    {
+    public ArrayList<Choice> getChoices() {
     	return choices;
     }
     
-    public String getText() {
+    public HashMap<String, Boolean> getConditionsToChange() {
+		return conditionsToChange;
+	}
+
+	public String getText() {
         return text;
     }
     
